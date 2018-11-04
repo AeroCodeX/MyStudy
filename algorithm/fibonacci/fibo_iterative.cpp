141c32d043e3b5caf42ba7435612e7a048970a46
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-05 00:08:14 +0900

    Update algorithm/fibonacci/fibo_iterative.cpp

--- algorithm/fibonacci/fibo_iterative.cpp
+++ algorithm/fibonacci/fibo_iterative.cpp
@@ -1,14 +1,10 @@
 ﻿#include "fibonacci.h"
-#include <string.h>
 long long fibo_iterative(int n)
 {
 	//! long long으로 표현할 수 있는 한계선.
 	if (92 < n) return -1;
 
-	long long fibo[93];
-	memset(fibo, 0, sizeof(fibo));
-	fibo[0] = 0;
-	fibo[1] = 1;
+	long long fibo[93] = { 0, 1 };
 	for (int i = 2; i <= n; i++)
 	{
 		fibo[i] = fibo[i - 1] + fibo[i - 2];
