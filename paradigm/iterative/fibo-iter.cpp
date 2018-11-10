1677769301e99338957d657b85815aafdeb34ecb
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-11 06:15:18 +0900

    iterative

---
+++ paradigm/iterative/fibo-iter.cpp
@@ -0,0 +1,12 @@
+﻿int fibo_iter(int n)
+{
+	//! 30 이상은 받지 않는다.
+	if (30 <= n) return -1;
+
+	int arr[30] = { 0, 1 };
+	for (int i = 2; i <= n; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	return arr[n];
+}
