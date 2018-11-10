ea631f41e981c424a76672ebc2afb7834fb200cb
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-11 05:47:53 +0900

    brute-force

---
+++ paradigm/brute-force/brute-force-1.cpp
@@ -0,0 +1,15 @@
+﻿#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> divisor;
+	int N = 1000000000;
+	for (int i = 1; i <= N; i++)
+	{
+		if (N % i == 0)
+		{
+			divisor.push_back(i);
+		}
+	}
+}
---
+++ paradigm/brute-force/brute-force-2.cpp
@@ -0,0 +1,17 @@
+﻿#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> divisor;
+	int N = 50;
+	int sqrt = 1000000000;
+	while (sqrt*sqrt < N) sqrt++;
+	for (int i = 1; i <= sqrt; i++)
+	{
+		if (N % i == 0)
+		{
+			divisor.push_back(i);
+		}
+	}
+}
