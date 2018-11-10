48026bbbebf19ce1cbc0ff475015e5f73c2b311d
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-11 06:16:33 +0900

    recursion

---
+++ paradigm/recursion/fibo-recursion.cpp
@@ -0,0 +1,11 @@
+﻿int fibo_recursion(int n)
+{
+	//! 30 이상은 받지 않는다.
+	if (30 <= n) return -1;
+
+	//! 기저사항
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+
+	return fibo_recursion(n - 1) + fibo_recursion(n - 2);
+}
