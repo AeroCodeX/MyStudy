a2bfd9f96f3ef1b74be04d09821ee8fcf438bda4
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-14 19:12:10 +0900

    Update brute-force-2.cpp
    sqrt와 N의 초기값이 달랐던 부분 수정

--- paradigm/brute-force/brute-force-2.cpp
+++ paradigm/brute-force/brute-force-2.cpp
@@ -4,14 +4,11 @@ using namespace std;
 int main()
 {
 	vector<int> divisor;
-	int N = 50;
-	int sqrt = 1000000000;
+	int N = 1000000000;
+	int sqrt = 0;
 	while (sqrt*sqrt < N) sqrt++;
 	for (int i = 1; i <= sqrt; i++)
 	{
-		if (N % i == 0)
-		{
-			divisor.push_back(i);
-		}
+		if (N % i == 0) divisor.push_back(i);
 	}
 }
