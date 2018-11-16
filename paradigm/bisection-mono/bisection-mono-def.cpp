c5c6e619c337936f0284b639989a355ebb7920d1
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-17 04:32:25 +0900

    bisection-mono

--- paradigm/bisection-mono/bisection-mono-def.cpp
+++ paradigm/bisection-mono/bisection-mono-def.cpp
@@ -1,14 +1,7 @@
 ﻿struct Range{ int s, int e };
 Range bisection(int x, bool isLeftward)
 {
-	if (isLeftward)
-	{	// left-ward
-		return bisection_min(x);
-	}
-	else
-	{	// right-ward
-		return bisection_max(x);
-	}
+	return (isLeftWard ? bisection_min(x) : bisection_max(x));
 }
 Range bisection_min(int x);
 Range bisection_max(int x);
