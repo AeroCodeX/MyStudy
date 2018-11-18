3403752804e7c6b91c35307eb61c23d3838aeda7
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-18 12:40:36 +0900

    Create 01.HelloWorld.kt

---
+++ kotlin/syntax/01.HelloWorld.kt
@@ -0,0 +1,8 @@
+﻿// 01. Hello, World!
+//  프로그램의 진입점은 main 이다.
+//  명령행 인자는 main 함수의 인자로 받는다.
+fun main(args:Array<String>)
+{
+    println("Hello, World!")
+    for(value in args) println(value)   // Range-based Loop
+}
