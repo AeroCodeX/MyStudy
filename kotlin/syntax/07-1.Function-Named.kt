7ea1c6198e9a53defb32912923e86e487f7f3db8
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-18 15:32:30 +0900

    함수

---
+++ kotlin/syntax/07-1.Function-Named.kt
@@ -0,0 +1,35 @@
+﻿// 07-1. named function
+
+//! 엄격한 함수선언
+//! Long add (int a = 3, int b = 1)
+fun add1(a:Int = 3, b:Int = 1) : Long
+{
+    return (a + b).toLong()
+}
+
+
+//! 간결한 함수선언 (하나의 표현식만 있을 때 가능)
+//! 반환형은 컴파일러가 유추한다.
+fun add2(a:Int, b:Int) = (a+b).toLong()
+
+
+//! 제네릭 <T>
+//! 가변인자 함수 (vararg)
+fun <T> printAll(vararg list : T)
+{
+    for(item in list) println(item)
+}
+
+
+//! 함수사용
+fun main(args:Array<String>)
+{
+    add1(3, 4)  // 3 + 4 = 7
+    add1(3)         // 3 + 1 = 4
+    add1(b=2)           // 3 + 2 = 5
+
+    printAll(4.2, 4.7, 5.9)
+}
+
+
+
---
+++ kotlin/syntax/07-2.Function-Anonymous).kt
@@ -0,0 +1,18 @@
+﻿// 07-2. Lambda(Anonymous) function
+
+fun main(args:Array<String>)
+{
+    val v1 = calc(5, 5, {a, b -> a + b})
+    val v2 = calc(5, 5, {a, b -> a * b})
+    println(v1) // 10
+    println(v2) // 25
+}
+
+fun calc(
+        val1: Int,
+        val2: Int,
+        exec: (p1: Int, p2: Int) -> Int   // 시그너쳐가 (int, int)이고 반환형이 int인 함수.
+) : Int
+{
+    return exec(val1, val2)
+}
