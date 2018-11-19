148086bbb6fe54ba42f0fcc317fd62abd9268fea
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-19 16:27:33 +0900

    데이터 클래스

---
+++ kotlin/syntax/15.DataClass.kt
@@ -0,0 +1,23 @@
+﻿// 15. DataClass
+
+fun main(args:Array<String>) {
+    var a = User("a", 23)   // (a, 23)
+    var b = a.copy(name="b")    // (b, 23)
+    var (c, d) = a
+    var e = b.component1()
+    var f = b.component2()
+
+    println(a) // print "User(name=a, age=23)"
+    println(b) // print "User(name=b, age=23)"
+    println(c) // a
+    println(d) // 23
+    println(e) // b
+    println(f) // 23
+}
+
+data class User(val name: String, val age: Int){ }
+//! equals() / hashCode() 자동 생성
+//! toString() 자동 생성
+//! (선언한 순서대로) n번째 요소의 대응되는 componentN() 자동 생성
+//! 일부 멤버필드만 변경한 뒤, 복사하는 copy() 자동 생성
+
