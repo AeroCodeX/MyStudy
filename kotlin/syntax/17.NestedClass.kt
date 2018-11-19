bd0979244ba7fdc13b28ab7984587b28bd9cc196
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-20 01:04:36 +0900

    중첩 클래스

---
+++ kotlin/syntax/17.NestedClass.kt
@@ -0,0 +1,24 @@
+﻿// 17. Nested Class
+
+fun main(args:Array<String>) {
+    val outer  = Outer()
+    val nested = Outer.Nested()
+
+    println(outer.call())   // print "outer"
+    println(nested.call())  // print "nested"
+}
+
+
+class Outer {
+    private val outerData = 0
+    fun call():String = "outer"
+
+    class Nested {
+        fun  call():String = "nested"
+        constructor()
+        {
+            //! Nested 클래스는 Outer 클래스의 멤버에 접근하지 못한다.
+            //! this.outerData = 3   -> 불가능
+        }
+    }
+}
