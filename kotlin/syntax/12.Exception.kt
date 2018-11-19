5ccd5205ed20357718af421b26173d3a4c2d9fe4
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-19 14:22:20 +0900

    익셉션

---
+++ kotlin/syntax/12.Exception.kt
@@ -0,0 +1,22 @@
+﻿// 12. Exception
+
+fun main(args:Array<String>) {
+    //! try-catch-finally 시작
+    //!     (1) try 블럭 시도
+    //!     (2) 예외발생시 catch 블럭으로 이동
+    //!     (3) 예외가 발생했든 안했든 finally 블럭으로 이동
+    //! try-catch=finally 종료
+
+    try {
+        val a = 1 / 0   // java.lang.ArithmeticException: / by zero
+    } catch (e: ArithmeticException) {
+        println("exception")
+    } finally {
+        println("done1")
+    }
+    println("done2")
+
+    // print "exception"
+    // print "done1"
+    // print "done2"
+}
