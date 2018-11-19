986c4d44dcbedce2cbd08d0242da89e8a56a367f
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-19 15:37:50 +0900

    외부 반복문 제어

---
+++ kotlin/syntax/13.Loop-with-Label.kt
@@ -0,0 +1,30 @@
+﻿// 13. Loop with Label
+
+fun main(args:Array<String>) {
+    //! 라벨에 continue 또는 break 명령을 걸면,
+    //! 라벨에서 가장 가까운 반복문에 영향을 끼친다.
+
+
+    outerLoop1@for(i in 1..3)
+        for (j in 0..1)
+        {
+            println("$i $j")
+            if (i == 2) break@outerLoop1 // 외부 반복문에 break
+        }
+    // print "1 0"
+    // print "1 1"
+    // print "2 0"
+
+
+    outerLoop2@for(i in 1..3)
+        for(j in 0..1)
+        {
+            println("$i $j")
+            if (i == 2) continue@outerLoop2 // 외부 반복문에 continue
+        }
+    // print "1 0"
+    // print "1 1"
+    // print "2 0"
+    // print "3 0"
+    // print "3 1"
+}
