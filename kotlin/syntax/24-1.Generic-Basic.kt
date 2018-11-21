449e663f215ee8f2ac6f85409eb52aeac3247813
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-22 01:55:58 +0900

    제네릭

--- kotlin/syntax/24-1.Generic-Basic.kt
+++ kotlin/syntax/24-1.Generic-Basic.kt
@@ -4,14 +4,10 @@ fun main(args: Array<String>)
 {
     val intBox = Box(3)
     val strBox = Box("str")
-    println1(intBox)
-    println1(strBox)
-    println2(intBox)
-    println2(strBox)
+    println(intBox)
+    println(strBox)
 
     // [Output]
-    //      3
-    //      str
     //      3
     //      str
 }
@@ -22,18 +18,9 @@ data class Box<T>(var data:T)
 
 
 // T는 어떤 데이터 타입이든 가능.
-// 인자는 Box<T> 타입으로 받음.
-fun <T>println1(box:Box<T>)
+// 대신 인자는 Box<T> 타입으로 받음.
+fun <T>println(box:Box<T>)
 {
     val innerData:T = box.data
     println(innerData)
-}
-
-
-// 처음부터 T를 Box<*> 이하로 제한. (상위 한계)
-// 인자는 Box<*> 이하로 제한된 T 타입으로 받음.
-fun <T:Box<*>>println2 (box:T)
-{
-    val innerData:Any? = box.data // box.data is Any?
-    println(innerData?.toString())
 }
