65acc4059b04d804e39599802c0b80ee42c8bc57
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-22 11:14:06 +0900

    클로져

---
+++ kotlin/syntax/27.Closure.kt
@@ -0,0 +1,11 @@
+﻿// 27. Closure
+
+fun main(vararg args: String) {
+    // 람다함수는 람다 밖의 변수를 수정할 수 있다.
+    var sum = 0
+    (0..10).forEach { sum += it }
+    println(sum)
+
+    // [Output]
+    //      55
+}
