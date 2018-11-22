4b9a122245d9b583072aab644fc3d91f7b33e6e5
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-22 12:09:20 +0900

    리시버

---
+++ kotlin/syntax/28-1.Function-Literals-With-Receiver.kt
@@ -0,0 +1,42 @@
+﻿// 28-1. Function-Literals-With-Receiver
+
+fun main(vararg args: String) {
+    val t1 = ADD {
+        // 리시버를 적용가능한 람다에서는,
+        // 람다가 객체를 * this 으로 참조할 수 있다.
+        // this 는 생략할 수 있다.
+        add(3)
+        add(2)
+        add(1)
+    }
+    val t2 = ADD2 {
+        // 평범한 람다함수에서는,
+        // 람다가 객체를 객체를 it 으로 참조한다.
+        // it 은 생략할 수 없다.
+        it.add(3)
+        it.add(2)
+        it.add(1)
+    }
+    println(t1.data)
+    println(t2.data)
+}
+
+class Adder(var data: Int = 0) {
+    fun add(n: Int) {
+        this.data += n
+    }
+}
+
+// 리시버를 적용할 수 있는 람다
+fun ADD(init: Adder.() -> Unit): Adder {
+    val adder = Adder()
+    adder.init()
+    return adder
+}
+
+// 평범한 람다
+fun ADD2(init:(Adder)->Unit):Adder{
+    val adder = Adder()
+    init(adder)
+    return adder
+}
