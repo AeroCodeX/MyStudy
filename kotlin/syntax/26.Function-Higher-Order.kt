56fca1a01aa4b08a781636d1edc88b1ac68d7e40
AeroCodeX <40631585+AeroCodeX@users.noreply.github.com>
2018-11-22 11:13:36 +0900

    고차함수

---
+++ kotlin/syntax/26.Function-Higher-Order.kt
@@ -0,0 +1,17 @@
+﻿// 26. Function-Higher-Order
+
+fun main(vararg args: String) {
+    execute { a, b -> a * b }
+    evenTest(3)
+    evenTest(3)()
+}
+
+
+// 고차함수(1) 함수를 인자로 받는 경우
+fun execute(a: Int = 1, b: Int = 2, f: (Int, Int) -> Int) = println(f(a, b))
+
+
+// 고차함수(2) 함수를 리턴하는 경우
+fun evenTest(n: Int): () -> Unit {
+    if (n % 2 == 0) return { println("isEven") } else return { println("isNotEven") }
+}
